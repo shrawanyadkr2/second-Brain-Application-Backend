728330b2ed05b9dd5b78d5d0c011aba17546fa99
shrawan <shrawan29yadav@gmail.com>
2025-08-18 15:52:22 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken"
+import express from "express";
+
+const app = express();
+
+app.post('/api/v1/signup', (req,res)=>{
+
+})
+
+app.post('/api/v1/signin',(req,res)=>{
+
+})
+
+app.post('/api/v1/content',(req,res)=>{
+
+})
+
+app.get('/api/v1/content',(req,res)=>{
+
+})
+
+app.delete('/api/v1/delete',(req,res)=>{
+
+})
+
+app.post('/api/v1/brain/shrare',(req,res)=>{
+
+})
+
+app.get('/api/v1/brain/:shareLink',(req,res)=>{
+
+})
